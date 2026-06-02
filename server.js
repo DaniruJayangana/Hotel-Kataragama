556f40d9d77e610f3d03c9920b8a36953797a75d
Daniru Jayangana <iamdanirujayangana@gmail.com>
2026-06-03 02:28:58 +0530

    Fix server.js

--- server.js
+++ server.js
@@ -40,9 +40,9 @@ if (process.env.NODE_ENV === 'production') {
     
     app.use(express.static(buildPath));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(buildPath, 'index.html'));
-    });
+   app.get('/*', (req, res) => {
+    res.sendFile(path.join(buildPath, 'index.html'));
+});
 }
 
 // 6. Catch-all for undefined routes
