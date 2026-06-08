f76321197d4e5b0e1419286d09152caf39733b22
Daniru Jayangana <iamdanirujayangana@gmail.com>
2026-06-08 16:45:04 +0530

    add low Stock Alert

--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -113,4 +113,14 @@ router.get('/inventory/all', authorize(['Manager', 'Staff']), asyncHandler(async
     res.status(200).json(allItems);
 }));
 
+// NEW: GET LOW STOCK INVENTORY
+router.get('/inventory/low-stock', authorize(['Manager', 'Staff']), asyncHandler(async (req, res) => {
+    // This assumes your InventoryItem model has 'current_stock' and 'reorder_level' fields
+    const lowStockItems = await InventoryItem.find({ 
+        $expr: { $lt: ["$current_stock", "$reorder_level"] } 
+    });
+    
+    res.status(200).json(lowStockItems);
+}));
+
 module.exports = router;
